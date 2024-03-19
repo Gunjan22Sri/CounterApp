b7266f081c98db777e6aae22884fa53ccb8c16d5
Gunjan Srivastava <gunjan.srivastava1986@gmail.com>
2024-03-19 10:31:50 +0530

    saved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'CounterComponents';
+  msg = "Counter Example";
+  count: number = 0;
+
+  increment() {
+    this.count++;
+  }
+  decrement() {
+    if (this.count > 0) {
+      this.count--;
+    }
+  }
+  reset() {
+    this.count = 0;
+  }
+
 }
